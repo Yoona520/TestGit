fb3ba5da6232e47a581818d999f5cd1d5cc70ead
zhanglei <525411942@qq.com>
2018-10-30 11:30:23 +0800

    firsr commit

---
+++ src/com/test/TestGit.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author 张磊
+ * @create 2018-10-30 上午 10:52
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("你好啊！");
+    }
+}
