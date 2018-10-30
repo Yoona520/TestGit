be32b14eb41bfde76929b4320e267a2bdd02384f
zhanglei <525411942@qq.com>
2018-10-30 12:17:10 +0800

    测试

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author 张磊
+ * @create 2018-10-30 下午 12:10
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
