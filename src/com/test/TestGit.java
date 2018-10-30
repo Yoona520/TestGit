02a54da31bb7277c43e9a300fa986d6c2d23d13b
zhanglei <525411942@qq.com>
2018-10-30 11:35:24 +0800

    second commit

--- src/com/test/TestGit.java
+++ src/com/test/TestGit.java
@@ -7,6 +7,7 @@ package com.test;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("你好啊！");
+        System.out.println("你好啊123！");
+    	
     }
 }
